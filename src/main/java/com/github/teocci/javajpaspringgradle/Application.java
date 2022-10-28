990cb95898c4e8c510c694028e778672ce8707fb
RTT PC <teocci@yandex.com>
2022-10-28 16:09:33 +0900

    tweak the configuration file to support hotswap and reload

--- src/main/java/com/github/teocci/javajpaspringgradle/Application.java
+++ src/main/java/com/github/teocci/javajpaspringgradle/Application.java
@@ -1,5 +1,6 @@
 package com.github.teocci.javajpaspringgradle;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class Application
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
+
 }
